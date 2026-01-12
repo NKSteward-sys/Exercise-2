c17364fc023fdc5dd506c08634fe74063003b7dc
Nathaniel Steward <nathanielsteward@Nathaniels-MacBook-Air-7.local>
2026-01-12 13:37:37 +0000

    File finding logic completely broken and requires revision

--- Code/Chemistry II - Programming option/Exercise 2 /PES graphing software.py
+++ Code/Chemistry II - Programming option/Exercise 2 /PES graphing software.py
@@ -10,26 +10,29 @@ def PES_Filereader(Folder):
 
         for r in np.arange(0.70, 1.95, 0.05): 
             for theta in range(70, 161, 1):
-                filenames.append(f"H2O.r{r:.2f}theta{theta}.0.out")
+                filenames.append(f"{UserFolder}/H2O.r{r:.2f}theta{theta}.0.out")
                 idx = idx + 1
 
     elif Folder == "H2Soutfiles": 
 
         for r in np.arange(0.60, 1.85, 0.05): 
             for theta in range(70, 161, 1):
-                filenames.append(f"H2S.r{r:.2f}theta{theta}.0.out")
+                filenames.append(f"{UserFolder}/H2S.r{r:.2f}theta{theta}.0.out")
                 idx = idx + 1
     
 
 
-
-
-    print(filenames)
+    for name in filenames: 
+        ORCA_Output = open(name, r)
+        Lines = []
+        for line in ORCA_Output: 
+            Lines.append(line)
+        print(Lines)
 
 
 
 if __name__ == "__main__":
-    Folder = input("What is the folder name?")
+    UserFolder = input("What is the folder name?")
 
-    PES_Filereader(Folder)
+    PES_Filereader(UserFolder)
 
