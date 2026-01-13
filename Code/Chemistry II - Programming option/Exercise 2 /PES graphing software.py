935abf6c618b4be8b499f370a9972b363617cb53
Nathaniel Steward <nathanielsteward@Nathaniels-MacBook-Air-7.local>
2026-01-13 14:41:37 +0000

    Finally got the program to extract only the energies as floats. TO DO: plot in 3d! Might wanna change the list into an array and add the radii and angles before plotting begins

--- Code/Chemistry II - Programming option/Exercise 2 /PES graphing software.py
+++ Code/Chemistry II - Programming option/Exercise 2 /PES graphing software.py
@@ -1,33 +1,52 @@
 import numpy as np
+import re
+import matplotlib.pyplot as plt
 
 #Explain this! 
 def PES_Filereader(Folder): 
-    
+
+    """The first step in reading these directories is generating the list of filenames. I have used loops to generate a list of filenames
+    using hardcoded parameters dependening on the input directory."""
+
     filenames = [] 
-    idx = 0
 
     if "H2Ooutfiles" in Folder:
 
-        for r in np.arange(0.70, 1.95, 0.05): 
-            for theta in range(70, 161, 1):
-                filenames.append(f"{Folder}/H2O.r{r:.2f}theta{theta}.0.out")
-                idx = idx + 1
+        r_range = np.arange(0.70, 1.95, 0.05)
+        molecule = "H2O"
 
     elif "H2Soutfiles" in Folder: 
 
-        for r in np.arange(0.60, 1.85, 0.05): 
-            for theta in range(70, 161, 1):
-                filenames.append(f"{Folder}/H2S.r{r:.2f}theta{theta}.0.out")
-                idx = idx + 1
+        r_range = np.arange(0.70, 1.85, 0.05)
+        molecule = "H2S"
     
+    else: 
+        print("Sorry, this isn't a directory I can parse. Make sure you've pasted the full file path.")
 
+    for r in r_range:
+        for theta in range(70, 161, 1):
+            filenames.append(f"{Folder}/{molecule}.r{r:.2f}theta{theta}.0.out")
+            
+    """Now that we have the list of filenames. We can iteratively open each file and read the contents. Using regular expressions, we search for the line with the energy
+      and extract the numerical value of the energy. We then close the file."""
 
+    Energies = []
     for name in filenames: 
-        ORCA_Output = open(name, 'r')
-        Lines = []
-        for line in ORCA_Output: 
-            Lines.append(line)
-        print(Lines[167])
+        Gaussian_Output = open(name, 'r')
+
+        for line in Gaussian_Output: 
+            match = re.search(r"E\(RHF\)\s*=?\s*([-+]?\d+\.\d+)", line)
+            if match: 
+                Energies.append(float(match.group(1)))
+                break
+
+        Gaussian_Output.close()
+    
+    print(len(Energies))
+
+
+
+
 
 
 
@@ -36,3 +55,4 @@ if __name__ == "__main__":
 
     PES_Filereader(UserFolder)
 
+#NB E is not in line 168 with H2O r = 1.9 and theta = 153 
